21ce11c5775ac382f085f9ef752be440d430b28c
Venera <venera_husainova@mail.ru>
2019-07-26 23:31:15 +0300

    server

--- js/script-filter.js
+++ js/script-filter.js
@@ -1,178 +1,168 @@
-const buttonFiter= document.querySelector('.button-filter'),
-      selectSorting = document.querySelector('#sorting'),
-      listProduct = document.querySelectorAll('.list-product'),
-      itemProduct = document.querySelectorAll('.item-product'),
-      sliderElement = document.getElementsByClassName('numbe-track'),
-      thumbRigt = document.querySelector('.numbe-thumb'),
-      slideRigt = document.querySelector('#slide-right'),
-      productName = document.querySelectorAll('.product-name-link');
-const saleProduct = document.querySelectorAll('.product-amount');
-const listFiller = document.querySelectorAll('.list-filler'),
-      Shocolat = document.querySelector('#shocolat'),
-      sugar = document.querySelector('#sugar'),
-      fruits = document.querySelector('#fruits'),
-      syrup = document.querySelector('#syrup'),
-      jam = document.querySelector('#jam');
+window.addEventListener("load", function () {
+  var buttonFiter = document.querySelector('.button-filter'),
+    selectSorting = document.querySelector('#sorting'),
+    listProduct = document.querySelectorAll('.list-product'),
+    itemProduct = document.querySelectorAll('.item-product'),
+    sliderElement = document.getElementsByClassName('numbe-track'),
+    thumbRigt = document.querySelector('.numbe-thumb'),
+    slideRigt = document.querySelector('#slide-right'),
+    productName = document.querySelectorAll('.product-name-link');
+  var saleProduct = document.querySelectorAll('.product-amount');
+  var listFiller = document.querySelectorAll('.list-filler'),
+    Shocolat = document.querySelector('#shocolat'),
+    sugar = document.querySelector('#sugar'),
+    fruits = document.querySelector('#fruits'),
+    syrup = document.querySelector('#syrup'),
+    jam = document.querySelector('#jam');
 
-function filter () {
-  buttonFiter.addEventListener('click', function () {
-    filterCheckbox ();
-    //filterCost ();
-   // filterForCost ();
-  })
-}
-filter ();
-
-/*Slider */
- function Slider () {
-  $( "#slider-range" ).slider({
-    range: true,
-    min: 100,
-    max: 800,
-    values: [200, 500],
-    slide: function( event, ui ) {
-      $( "#amount" ).val( ui.values[ 0 ] + " руб. - " + ui.values[ 1 ]  + " руб."); 
-    },
-    stop: function( event, ui ) { 
-      $( "#slider-range" ).slider({
-        values: [ ui.values[ 0 ], ui.values[ 1 ] ]
-      })
-      var slidMin = $( "#slider-range" ).slider( "values", 0 );
-      var slidMax = $( "#slider-range" ).slider( "values", 1 );
-      console.log(slidMax, slidMin);
-      console.log($(".itemProduct"));
-      $.each($(".itemProduct"), function () {
-        console,log("перебор");
-         if (+slidMax > +slidMin) {
-console.log("ok");
-         } 
-      })
-    }
-  });
-  $( "#amount" ).val( $( "#slider-range" ).slider( "values", 0 ) +
-    " руб. - " + $( "#slider-range" ).slider( "values", 1 ) + " руб.");
-     
-};
-Slider ();
-console.log(Slider ());
-
-  let test = Slider ();
- // console.log(test);
-  
-
-function filterCheckbox () {
-  itemProduct.forEach(function (item) { 
-    item.classList.remove('show');
-  })
-  productName.forEach(function (item, i) {
-    if (Shocolat.checked){
-      if(item.textContent.indexOf('шоколад') !== -1) {
-        console.log('chockolat')
-        itemProduct[i].classList.add('show');
-      }
-     }
-     if (sugar.checked){
-      if(item.textContent.indexOf('присып') !== -1) {
-        itemProduct[i].classList.add('show');
-      }
-     }
-     if (fruits.checked){
-      if(item.textContent.indexOf('апельс') !== -1 || item.textContent.indexOf('клубнич') !== -1 || item.textContent.indexOf('бруснич') !== -1 || item.textContent.indexOf('лимон') !== -1 || item.textContent.indexOf('чернич') !== -1) {
-        itemProduct[i].classList.add('show');
-      }
-     }
-     if (syrup.checked){
-      if(item.textContent.indexOf('сироп') !== -1 || item.textContent.indexOf('подлив') !== -1) {
-        itemProduct[i].classList.add('show');
-      }
-     }
-     if (jam.checked){
-      if(item.textContent.indexOf('джем') !== -1) {
-        itemProduct[i].classList.add('show');
-      }
-     }
-     if (jam.checked === false && syrup.checked === false && fruits.checked === false && sugar.checked === false && Shocolat.checked === false) {
-      itemProduct[i].classList.add('show');
-     }
-  })
-}
-
-function filterForCost () {
-  let curent = 0;
-  for (let i = 0; i < itemProduct.length - 2; i++){
-  let min = itemProduct[i].querySelector('.product-amount').textContent;
-  console.log("перебор элементов" + min);
-  curent++;
-  for (let j = curent; j < itemProduct.length - 1; j++) {
-    if (min >= itemProduct[j].querySelector('.product-amount').textContent) {
-        min = itemProduct[j].querySelector('.product-amount').textContent;
-        console.log('min!' + min);
-        let swep = itemProduct[i];
-        console.log(itemProduct);
-        itemProduct[i] = itemProduct[j];
-        itemProduct[j] = swep;
-        console.log(itemProduct);
-    } else {
-      console.log("все также" + min);
-    }
-}
+  function filter() {
+    buttonFiter.addEventListener('click', function () {
+      filterCheckbox();
+      //filterCost ();
+      // filterForCost ();
+    })
   }
-}
-  
+  filter();
 
+  /*Slider */
+  function Slider() {
+    $("#slider-range").slider({
+      range: true,
+      min: 100,
+      max: 800,
+      values: [200, 500],
+      slide: function (event, ui) {
+        $("#amount").val(ui.values[0] + " руб. - " + ui.values[1] + " руб.");
+      },
+      stop: function (event, ui) {
+        $("#slider-range").slider({
+          values: [ui.values[0], ui.values[1]]
+        })
+        var slidMin = $("#slider-range").slider("values", 0);
+        var slidMax = $("#slider-range").slider("values", 1);
+        console.log(slidMax, slidMin);
+        console.log($(".itemProduct"));
+        $.each($(".itemProduct"), function () {
+          log("перебор");
+          if (+slidMax > +slidMin) {
+            console.log("ok");
+          }
+        })
+      }
+    });
+    $("#amount").val($("#slider-range").slider("values", 0) +
+      " руб. - " + $("#slider-range").slider("values", 1) + " руб.");
 
-console.log();
+  };
+  Slider();
 
-/*
-function filterCatalog () {
+  function filterCheckbox() {
+    itemProduct.forEach(function (item) {
+      item.classList.remove('show');
+    })
+    productName.forEach(function (item, i) {
+      if (Shocolat.checked) {
+        if (item.textContent.indexOf('шоколад') !== -1) {
+          itemProduct[i].classList.add('show');
+        }
+      }
+      if (sugar.checked) {
+        if (item.textContent.indexOf('присып') !== -1) {
+          itemProduct[i].classList.add('show');
+        }
+      }
+      if (fruits.checked) {
+        if (item.textContent.indexOf('апельс') !== -1 || item.textContent.indexOf('клубнич') !== -1 || item.textContent.indexOf('бруснич') !== -1 || item.textContent.indexOf('лимон') !== -1 || item.textContent.indexOf('чернич') !== -1) {
+          itemProduct[i].classList.add('show');
+        }
+      }
+      if (syrup.checked) {
+        if (item.textContent.indexOf('сироп') !== -1 || item.textContent.indexOf('подлив') !== -1) {
+          itemProduct[i].classList.add('show');
+        }
+      }
+      if (jam.checked) {
+        if (item.textContent.indexOf('джем') !== -1) {
+          itemProduct[i].classList.add('show');
+        }
+      }
+      if (jam.checked === false && syrup.checked === false && fruits.checked === false && sugar.checked === false && Shocolat.checked === false) {
+        itemProduct[i].classList.add('show');
+      }
+    })
+  }
+
+  function filterForCost() {
     let curent = 0;
     for (let i = 0; i < itemProduct.length - 2; i++) {
-      let minSale = +itemProduct[i].querySelector('.product-amount').textContent; 
-      curent++; 
-    for (let j = curent; j < itemProduct.length - 1; j++) {
-        if (minSale > +itemProduct[j].querySelector('.product-amount').textContent) {
-            console.log('min!');
-            minSale = +itemProduct[j].querySelector('.product-amount').textContent;
-            let swep = itemProduct[i];
-            itemProduct[i] = itemProduct[j];
-            itemProduct[j] = swep;
+      let min = itemProduct[i].querySelector('.product-amount').textContent;
+      curent++;
+      for (let j = curent; j < itemProduct.length - 1; j++) {
+        if (min >= itemProduct[j].querySelector('.product-amount').textContent) {
+          min = itemProduct[j].querySelector('.product-amount').textContent;
+          console.log('min!' + min);
+          let swep = itemProduct[i];
+          console.log(itemProduct);
+          itemProduct[i] = itemProduct[j];
+          itemProduct[j] = swep;
+          console.log(itemProduct);
+        } else {
+          console.log("все также" + min);
         }
-        
-    }
-    console.log(itemProduct);
-      console.log(minSale);
-    }
-    console.log(itemProduct.length);
-    console.log(itemProduct);
-    console.log(saleProduct);
-    function sorting () {
-      let itemSorting = selectSorting.options[selectSorting.selectedIndex].value;
-      console.log(itemSorting);
-     
-      switch(itemSorting) {
-          case itemSorting = 'Сначала дорогие': console.log('ok');
-          break;
-          case itemSorting = 'Сначала недорогие': break;
-          case itemSorting = 'по жирности': break;
-
       }
     }
-    
-    sorting();
-    
-}
+  }
 
-buttonFiter.addEventListener('click', function (evt) {
-    evt.preventDefault;
-    filterCatalog ();
-})
+  /*
+  function filterCatalog () {
+      let curent = 0;
+      for (let i = 0; i < itemProduct.length - 2; i++) {
+        let minSale = +itemProduct[i].querySelector('.product-amount').textContent; 
+        curent++; 
+      for (let j = curent; j < itemProduct.length - 1; j++) {
+          if (minSale > +itemProduct[j].querySelector('.product-amount').textContent) {
+              console.log('min!');
+              minSale = +itemProduct[j].querySelector('.product-amount').textContent;
+              let swep = itemProduct[i];
+              itemProduct[i] = itemProduct[j];
+              itemProduct[j] = swep;
+          }
+          
+      }
+      console.log(itemProduct);
+        console.log(minSale);
+      }
+      console.log(itemProduct.length);
+      console.log(itemProduct);
+      console.log(saleProduct);
+      function sorting () {
+        let itemSorting = selectSorting.options[selectSorting.selectedIndex].value;
+        console.log(itemSorting);
+       
+        switch(itemSorting) {
+            case itemSorting = 'Сначала дорогие': console.log('ok');
+            break;
+            case itemSorting = 'Сначала недорогие': break;
+            case itemSorting = 'по жирности': break;
 
-  const slider = new Slider({
-   element: sliderElement,
-   max: 500
+        }
+      }
+      
+      sorting();
+      
+  }
+
+  buttonFiter.addEventListener('click', function (evt) {
+      evt.preventDefault;
+      filterCatalog ();
   })
-  sliderElement.addEventListener('slide', function(event) {
-    slideRigt.textContent = event.detail;
-  })
-*/
 
+    const slider = new Slider({
+     element: sliderElement,
+     max: 500
+    })
+    sliderElement.addEventListener('slide', function(event) {
+      slideRigt.textContent = event.detail;
+    })
+  */
+});
--- js/script-popup-slider.js
+++ js/script-popup-slider.js
@@ -1,72 +1,80 @@
-window.onload = function(){
-  const listSlider = document.querySelector('.list-sliders'),
-      itemsSlider = listSlider.children,
-      fonBody = document.querySelector('#fon'),
-      listBreadcrumbs = document.querySelector('.list-breadcrumbs'),
-      itemsBreadcrumb = listBreadcrumbs.children,
-      popup = document.querySelector('.feedback'),
-      backgroundPopup = document.querySelector('.background'),
-      openPopup = document.querySelector('.map-feedback'),
-      closePopup = popup.querySelector('.close-up'),
-      dispatchFormFeedback = popup.querySelector('.feeedback-dispatch'),
-      userNamePopup = popup.querySelector('#user-name'),
-      userEmailPopup = popup.querySelector('#email-feedback'),
-      commentPopup = popup.querySelector('#comment');
+window.addEventListener("load", function () {
+  var listSlider = document.querySelector('.list-sliders'),
+    itemsSlider = listSlider.children,
+    fonBody = document.querySelector('#fon'),
+    listBreadcrumbs = document.querySelector('.list-breadcrumbs'),
+    itemsBreadcrumb = listBreadcrumbs.children,
+    popup = document.querySelector('.feedback'),
+    backgroundPopup = document.querySelector('.background'),
+    openPopup = document.querySelectorAll('.map-feedback'),
+    closePopup = popup.querySelectorAll('.close-up'),
+    dispatchFormFeedback = popup.querySelector('.feeedback-dispatch'),
+    userNamePopup = popup.querySelector('#user-name'),
+    userEmailPopup = popup.querySelector('#email-feedback'),
+    commentPopup = popup.querySelector('#comment');
 
-const switchSlider = function (breadcrumbs) {
-  const addSwitchSliderClick = function (breadcrumb, element, fon) {
-    breadcrumb.addEventListener('click', function () {
-      for (let j = 0; j < itemsSlider.length; j++) {
-        if (itemsSlider[j].classList.contains('show')) {
-          itemsSlider[j].classList.remove('show');
-          itemsBreadcrumb[j].classList.remove('show-background-white');
+  var switchSlider = function (breadcrumbs) {
+    var addSwitchSliderClick = function (breadcrumb, element, fon) {
+      breadcrumb.addEventListener('click', function () {
+        for (let j = 0 ; j < itemsSlider.length ; j++) {
+          if (itemsSlider[j].classList.contains('show')) {
+            itemsSlider[j].classList.remove('show');
+            itemsBreadcrumb[j].classList.remove('show-background-white');
+          }
         }
-      }
-      fonBody.classList.remove('fon-1', 'fon-shine-1', 'fon-2', 'fon-shine-2', 'fon-3', 'fon-shine-3');
-      switch(fon) {
-        case fon = 0: fonBody.classList.add('fon-1', 'fon-shine-1');
-                    break;
-        case fon = 1: fonBody.classList.add('fon-2', 'fon-shine-2');
-                    break;
-        case fon = 2: fonBody.classList.add('fon-3', 'fon-shine-3');
-                    break;
-      }   
-    element.classList.add('show');
-    breadcrumb.classList.add('show-background-white');
-    });
-  }  
-      for (let i = 0; i < breadcrumbs.length; i++) {
-       addSwitchSliderClick(breadcrumbs[i], itemsSlider[i], i);
+        fonBody.classList.remove('fon-1', 'fon-shine-1', 'fon-2', 'fon-shine-2', 'fon-3', 'fon-shine-3');
+        switch (fon) {
+          case fon = 0:
+            fonBody.classList.add('fon-1', 'fon-shine-1');
+            break;
+          case fon = 1:
+            fonBody.classList.add('fon-2', 'fon-shine-2');
+            break;
+          case fon = 2:
+            fonBody.classList.add('fon-3', 'fon-shine-3');
+            break;
+        }
+        element.classList.add('show');
+        breadcrumb.classList.add('show-background-white');
+      });
+    }
+    for (let i = 0; i < breadcrumbs.length; i++) {
+      addSwitchSliderClick(breadcrumbs[i], itemsSlider[i], i);
+
+    }
+  }
+  switchSlider(itemsBreadcrumb);
+
+  function OpenPopup() {
+    openPopup.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+        popup.classList.add('show-popup');
+        ClosePopup();
+        backgroundPopup.classList.add('show-background');
+      })
+    })
 
   }
-}  
-switchSlider(itemsBreadcrumb);
 
-  function OpenPopup () {
-  openPopup.addEventListener('click', function () {
-    popup.classList.add('show-popup');
-    ClosePopup();
-    backgroundPopup.classList.add('show-background');
-  })
-}
-function ClosePopup () {
-  closePopup.addEventListener('click', function () {
-    popup.classList.remove('show-popup');
-    backgroundPopup.classList.remove('show-background');
-  })
-}
-OpenPopup ();
-
-const workFormFeedback = function () {
-  if (userNamePopup.pattern === ' ' || userNamePopup.pattern === '[0-9]') {
-    userNamePopup.addEventListener('focus', function () {
+  function ClosePopup() {
+    closePopup.forEach(function (btn) {
+      btn.addEventListener('click', function () {
+        popup.classList.remove('show-popup');
+        backgroundPopup.classList.remove('show-background');
+      })
     })
   }
-  commentPopup.addEventListener('click', function () {
-    commentPopup.value = " ";
-  })
-}
+  OpenPopup();
 
-workFormFeedback();
+  var workFormFeedback = function () {
+    if (userNamePopup.pattern === ' ' || userNamePopup.pattern === '[0-9]') {
+      userNamePopup.addEventListener('focus', function () {})
+    }
+    commentPopup.addEventListener('click', function () {
+      commentPopup.value = " ";
+    })
+  }
 
-}
+  workFormFeedback();
+
+});
